61e98ee4bab79a6b39199e7c89a5dcd232a14885
davidbogue <davebogue@gmail.com>
2013-10-12 17:19:51 -0500

    Work on Welcome Screen

--- MyEverydayBaby/build/source/r/debug/com/myeverydaybaby/R.java
+++ MyEverydayBaby/build/source/r/debug/com/myeverydaybaby/R.java
@@ -29,9 +29,10 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
+        public static final int action_settings=0x7f09000d;
         public static final int babyNameText=0x7f090006;
         public static final int birthdayText=0x7f090007;
+        public static final int button=0x7f09000c;
         public static final int camera_button=0x7f09000b;
         public static final int content_frame=0x7f090002;
         public static final int drawer_layout=0x7f090001;
@@ -66,19 +67,22 @@ public final class R {
         /**  From: file:/Users/davidbogue/AndroidStudioProjects/MyEverydayBabyProject/MyEverydayBaby/src/main/res/values/strings.xml 
          */
         public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int before_we_begin=0x7f060002;
-        public static final int birthday=0x7f060003;
-        public static final int drawer_close=0x7f060004;
-        public static final int drawer_open=0x7f060005;
-        public static final int from_camera=0x7f060006;
-        public static final int from_gallery=0x7f060007;
-        public static final int hello_world=0x7f060008;
-        public static final int hi=0x7f060009;
-        public static final int name=0x7f06000a;
-        public static final int not_available=0x7f06000b;
-        public static final int title_activity_dashboard=0x7f06000c;
-        public static final int title_activity_welcome=0x7f06000d;
+        public static final int album_name=0x7f060001;
+        public static final int app_name=0x7f060002;
+        public static final int before_we_begin=0x7f060003;
+        public static final int birthday=0x7f060004;
+        public static final int drawer_close=0x7f060005;
+        public static final int drawer_open=0x7f060006;
+        public static final int error_field_required=0x7f060007;
+        public static final int from_camera=0x7f060008;
+        public static final int from_gallery=0x7f060009;
+        public static final int get_started=0x7f06000a;
+        public static final int hello_world=0x7f06000b;
+        public static final int hi=0x7f06000c;
+        public static final int name=0x7f06000d;
+        public static final int not_available=0x7f06000e;
+        public static final int title_activity_dashboard=0x7f06000f;
+        public static final int title_activity_welcome=0x7f060010;
     }
     public static final class style {
         /**  From: file:/Users/davidbogue/AndroidStudioProjects/MyEverydayBabyProject/MyEverydayBaby/src/main/res/values/styles.xml 
--- MyEverydayBaby/src/main/java/com/myeverydaybaby/WelcomeActivity.java
+++ MyEverydayBaby/src/main/java/com/myeverydaybaby/WelcomeActivity.java
@@ -2,33 +2,59 @@ package com.myeverydaybaby;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
-import android.app.DialogFragment;
 import android.widget.TextView;
 
 import com.myeverydaybaby.fragments.DatePickerFragment;
+import com.myeverydaybaby.models.Baby;
+import com.myeverydaybaby.persistence.BabyDAO;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class WelcomeActivity extends Activity implements DatePickerDialog.OnDateSetListener {
 
     private static final int ACTION_TAKE_SMALL_PHOTO = 2;
+    private static final int ACTION_PICK_PHOTO = 3;
+    private static final String BITMAP_STORAGE_KEY = "viewbitmap";
+    private static final String JPEG_FILE_PREFIX = "IMG_";
+    private static final String JPEG_FILE_SUFFIX = ".jpg";
+    private static final String CAMERA_DIR = "/dcim/";
 
     private ImageView mImageView;
     private Bitmap mImageBitmap;
     private TextView mBirthdayText;
     private TextView mNameText;
+    private String profilePhotoPath;
+
+    private SaveBabyTask saveBabyTask;
+    private BabyDAO babyDAO;
 
     private Button.OnClickListener mTakePicOnClickListener =
             new Button.OnClickListener() {
@@ -51,6 +77,8 @@ public class WelcomeActivity extends Activity implements DatePickerDialog.OnDate
 
         Button picBtn = (Button) this.findViewById(R.id.camera_button);
         setBtnListenerOrDisable( picBtn, mTakePicOnClickListener, MediaStore.ACTION_IMAGE_CAPTURE );
+
+        babyDAO = new BabyDAO(this);
     }
 
     /**
@@ -68,6 +96,25 @@ public class WelcomeActivity extends Activity implements DatePickerDialog.OnDate
     }
 
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case ACTION_TAKE_SMALL_PHOTO: {
+                if (resultCode == this.RESULT_OK) {
+                    handleSmallCameraPhoto(data);
+                }
+                break;
+            }
+            case ACTION_PICK_PHOTO:{
+                if(resultCode == this.RESULT_OK){
+                    handleGalleryPhoto(data);
+                }
+                break;
+            }
+        } // switch
+    }
+
+
     // DATE PICKER CODE
 
     public void showDatePickerDialog(View v) {
@@ -82,28 +129,152 @@ public class WelcomeActivity extends Activity implements DatePickerDialog.OnDate
     }
 
 
+    // GALLERY RELATED CODE
+    public void pickPictureFromGallery(View view){
+        Intent i = new Intent( Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, ACTION_PICK_PHOTO);
+    }
+
+    private void handleGalleryPhoto(Intent data){
+        Uri selectedImage = data.getData();
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+        Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+        cursor.moveToFirst();
+
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+
+        mImageBitmap =  getScaledBitmap(picturePath);
+        if(mImageBitmap != null){
+            profilePhotoPath = picturePath;
+            mImageView.setImageBitmap( mImageBitmap);
+        }
+    }
+
+    private Bitmap getScaledBitmap(String pathName){
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(pathName, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Matrix matrix = new Matrix();
+        try {
+            ExifInterface exif = new ExifInterface(pathName);
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+            if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                matrix.postRotate(90);
+            }
+            else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                matrix.postRotate(180);
+            }
+            else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                matrix.postRotate(270);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeFile(pathName, bmOptions);
+        if(bitmap != null){
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true); // rotating bitmap
+        }
+        return bitmap;
+    }
+
     // CAMERA RELATED CODE
 
     private void dispatchTakePictureIntent(int actionCode) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, actionCode);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case ACTION_TAKE_SMALL_PHOTO: {
-                if (resultCode == this.RESULT_OK) {
-                    handleSmallCameraPhoto(data);
-                }
-                break;
-            }
-        } // switch
+        File f;
+        try {
+            f = setUpPhotoFile();
+            profilePhotoPath = f.getAbsolutePath();
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+            startActivityForResult(takePictureIntent, actionCode);
+        } catch (IOException e) {
+            e.printStackTrace();
+            f = null;
+        }
     }
 
     private void handleSmallCameraPhoto(Intent intent) {
         Bundle extras = intent.getExtras();
         mImageBitmap = (Bitmap) extras.get("data");
+        mImageView.setImageBitmap(mImageBitmap);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+        File albumF = getAlbumDir();
+        File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
+        return imageF;
+    }
+
+    private File getAlbumDir() {
+        File storageDir = null;
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+
+            storageDir = new File (
+                    Environment.getExternalStorageDirectory()
+                            + CAMERA_DIR
+                            + getString(R.string.album_name)
+            );
+
+            if (storageDir != null) {
+                if (! storageDir.mkdirs()) {
+                    if (! storageDir.exists()){
+                        Log.d("CameraSample", "failed to create directory");
+                        return null;
+                    }
+                }
+            }
+
+        } else {
+            Log.v(getString(R.string.app_name), "External storage is not mounted READ/WRITE.");
+        }
+
+        return storageDir;
+    }
+
+
+    private File setUpPhotoFile() throws IOException {
+
+        File f = createImageFile();
+        profilePhotoPath = f.getAbsolutePath();
+
+        return f;
+    }
+
+    // Some lifecycle callbacks so that the image can survive orientation change
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mImageBitmap = savedInstanceState.getParcelable(BITMAP_STORAGE_KEY);
         mImageView.setImageBitmap(mImageBitmap);
     }
 
@@ -141,6 +312,86 @@ public class WelcomeActivity extends Activity implements DatePickerDialog.OnDate
 
     // SAVE AND CONTINUE CODE
     public void saveAndContinue(View v){
+        if(saveBabyTask != null){
+            return;
+        }
+        if(validateSave()){
+            Baby b = new Baby();
+            b.setName( mNameText.getText().toString() );
+            b.setBirthday( getBirthdayAsLong() );
+            b.setPicture( profilePhotoPath );
+            saveBabyTask = new SaveBabyTask();
+            saveBabyTask.execute( (Baby) b );
+        }
+    }
+
+    private long getBirthdayAsLong(){
+        String bdayText = mBirthdayText.getText().toString();
+        if( TextUtils.isEmpty(bdayText) ){
+            return System.currentTimeMillis();
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            Date date = simpleDateFormat.parse(bdayText);
+            return date.getTime();
+        } catch (ParseException e) {
+            return System.currentTimeMillis();
+        }
+    }
+
+    private boolean validateSave(){
+        mNameText.setError(null);
+        mBirthdayText.setError(null);
+        boolean valid = true;
+
+        if(TextUtils.isEmpty( mNameText.getText().toString())){
+            mNameText.setError( getString(R.string.error_field_required));
+            valid = false;
+        }
+        if(TextUtils.isEmpty( mBirthdayText.getText().toString())){
+            mBirthdayText.setError( getString(R.string.error_field_required));
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    /**
+     * Represents an asynchronous task used to load user data from the SQLite database
+     * the user.
+     */
+    public class SaveBabyTask extends AsyncTask<Baby, Void, Baby> {
+        String errorMessage;
+
+        @Override
+        protected Baby doInBackground(Baby... babies) {
+            Baby baby = babies[0];
+            try {
+                babyDAO.open();
+                babyDAO.createBaby(baby);
+            } catch (SQLException e) {
+                errorMessage = "There was an unexpected error. Please try again.";
+                return null;
+            }finally {
+                babyDAO.close();
+            }
+            return baby;
+        }
+
+        @Override
+        protected void onPostExecute(final Baby baby) {
+            saveBabyTask = null;
+            if (baby != null) {
+                Intent intent = new Intent(WelcomeActivity.this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            saveBabyTask = null;
+        }
 
     }
 
